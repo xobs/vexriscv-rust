7ce847ee3c6131956c7acf10aa2b0cf0b35476d0
Vadim Kaushan <admin@disasm.info>
2020-06-14 14:32:33 +0300

    Fix register docs

--- src/register/vmim.rs
+++ src/register/vmim.rs
@@ -1,4 +1,4 @@
-//! uscratch register
+//! Machine IRQ Mask register
 
 read_csr_as_usize!(0xBC0, __read_vmim);
 write_csr_as_usize!(0xBC0, __write_vmim);
--- src/register/vsip.rs
+++ src/register/vsip.rs
@@ -1,4 +1,4 @@
-//! uscratch register
+//! Supervisor IRQ Pending register
 
 read_csr_as_usize!(0xDC0, __read_vsip);
 write_csr_as_usize!(0xDC0, __write_vsip);
